08db0d4701af2c0c29a63046cbd9d3830e0b7808
BKiptoo <bettsonk@gmail.com>
2025-08-27 14:52:37 +0300

    GAME-9 --Removed echo parameter from database engine creation in init_db function

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ def init_db():
     max_attempts = 3
 
     while attempt < max_attempts:
-        engine = create_engine('sqlite:///my_first_go88.db', echo=True)
+        engine = create_engine('sqlite:///my_first_go88.db')
 
         if engine is not None:
             # Create tables
