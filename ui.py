354790157b50ee0b601a3db5b185bd89820c0d68
Jane Nduku Kimei <Janetnduku519@gmail.com>
2025-08-27 21:22:03 +0300

    GAME-11 <message>

--- ui.py
+++ ui.py
@@ -4,3 +4,14 @@ from models import save_game
 def print_board(board):
     # Print column numbers
     print("  " + " ".join(str(i) for i in range(board.size)))
+     # Print each row with row number
+    for row in range(board.size):
+        symbols = []
+        for cell in board.board[row]:
+            if cell == 0:
+                symbols.append(".")
+            elif cell == 1:
+                symbols.append("B")
+            else:
+                symbols.append("W")
+        print(f"{row} " + " ".join(symbols))
